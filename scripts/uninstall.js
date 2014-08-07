98258d2122cdbaf918a3aee6334f0ba44987d195
Alastair Paragas <alastairparagas@gmail.com>
2014-08-06 20:03:38 -0400

    Squash unlink-no callback error

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -6,6 +6,11 @@ var fs = require('fs'), path = require('path'), cwd = process.cwd();
 
 var minifyJsPath = path.join(cwd, '..', '..', 'hooks', 'after_prepare', 'cordova-minify.js');
 
-fs.unlink(minifyJsPath);
-console.log('Uninstalled hooks: ', minifyJsPath);
+fs.unlink(minifyJsPath, function(error){
+    if(error == undefined){
+        console.log('Uninstalled hooks: ', minifyJsPath);
+    }else{
+        console.log('Cannot find hook to remove at ' + minifyJsPath + '. It may already have been removed!');
+    }
+});
 console.log('Uninstalled cordova-minify. We are sad to see you go! See you soon!');
