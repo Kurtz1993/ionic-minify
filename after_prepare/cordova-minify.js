ef4df7c329034c268d47ebfd5b6dc94c90ea3622
Alastair Paragas <alastairparagas@gmail.com>
2015-05-10 09:10:32 -0400

    Switch from image-min to imagemin dependency

--- after_prepare/cordova-minify.js
+++ after_prepare/cordova-minify.js
@@ -60,22 +60,22 @@ function compress(file) {
             var result = cssMinifier.minify(source);
             fs.writeFileSync(file, result, 'utf8');
             break;
-        // Image options https://github.com/kevva/imagemin
+        // Image options https://github.com/imagemin/imagemin
         case '.svg':
             console.log('Minifying SVG File: ' + file);
-            // svgGo options https://github.com/kevva/imagemin-svgo
+            // svgGo options https://github.com/imagemin/imagemin#svgo
             imagemin.src(file).dest(file).use(ImageMin.svgo());
             break;
         case '.gif':
             console.log('Minifying GIF File: ' + file);
-            // GifSicle options https://github.com/kevva/imagemin-gifsicle
+            // GifSicle options https://github.com/imagemin/imagemin#gifsicle
             imagemin.src(file).dest(file).use(ImageMin.gifsicle({
-                interlace: true
+                interlaced: true
             }));
             break;
         case '.png':
             console.log('Minifying PNG File: ' + file);
-            // OptiPNG options https://github.com/kevva/imagemin-optipng
+            // OptiPNG options https://github.com/imagemin/imagemin#optipng
             imagemin.src(file).dest(file).use(ImageMin.optipng({
                 optimizationLevel: 2
             }));
@@ -83,7 +83,7 @@ function compress(file) {
         case '.jpg':
         case '.jpeg':
             console.log('Minifying JPEG File: ' + file);
-            // jpegTran options https://github.com/kevva/imagemin-jpegtran
+            // jpegTran options https://github.com/imagemin/imagemin#jpegtran
             imagemin.src(file).dest(file).use(ImageMin.jpegtran({
                 progressive: true
             }));
