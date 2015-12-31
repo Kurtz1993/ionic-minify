a1cf4e040defac84135a8b5f2dd3b60fd9be50db
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2015-12-30 19:08:49 -0600

    Add valid UglifyJS configurations declarations.

--- lib/ionicMinify.d.ts
+++ lib/ionicMinify.d.ts
@@ -25,6 +25,83 @@ declare interface IMConfig{
   showErrStack: boolean
 }
 
+declare interface UJSCompressorOptions {
+  /**
+   * Join consecutive simple statements using the comma operator.
+   */
+  sequences?: boolean,
+  /**
+   * Rewrite property access using the dot notation.
+   */
+  properties?: boolean,
+  /**
+   * Remove unreachable code.
+   */
+  dead_code?: boolean,
+  /**
+   * Remove debugger; statements.
+   */
+  drop_debugger?: boolean,
+  /**
+   * Apply optimizations for if's and conditional expressions.
+   */
+  conditionals?: boolean,
+  /**
+   * Attempt to evaluate constant expressions.
+   */
+  evaluate?: boolean,
+  /**
+   * Various optimizations for boolean context.
+   */
+  booleans?: boolean,
+  /**
+   * Optimizations for do, while and for loops when we can statically determine the condition.
+   */
+  loops?: boolean,
+  /**
+   * Drop unreferenced functions and variables.
+   */
+  unused?: boolean,
+  /**
+   * Hoist function declarations.
+   */
+  hoist_funs?: boolean,
+  /**
+   * Optimizations for if/return and if/continue.
+   */
+  if_return?: boolean,
+  /**
+   * Join consecutive var statements.
+   */
+  join_vars?: boolean,
+  /**
+   * Small optimization for sequences.
+   */
+  cascade?: boolean,
+  /**
+   * Negate Immediately-Invoked Function Expressions where the return value is
+   * discarded, to avoid the parens that the code generator would insert. 
+   */
+  negate_iife?: boolean,
+  /**
+   * Discard calls to console.* functions.
+   */
+  drop_console?: boolean
+}
+
+declare interface UJSConfig {
+  /**
+   * Display compressor warnings.
+   * @default false
+   */
+  warnings?: string,
+  /**
+   * UglifyJS custom compressor options.
+   * @default {}
+   */
+  compress?: UJSCompressorOptions
+}
+
 declare module "ionic-minify"{
   export class Minifier {
     /**
