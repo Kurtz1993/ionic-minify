abc0354a20d9c351684ce330a74a3ae7e7ab6f33
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-01-07 18:26:05 -0600

    Change trigger flag to --minify
    - --release flag was breaking the android build in latest build tools
      so it's now replaced by the --minify flag.
    
    See #13 for more information.
    Thanks to @aaronksaunders for spotting this.

--- after_prepare/ionic-minify.ts
+++ after_prepare/ionic-minify.ts
@@ -6,7 +6,7 @@ import {Minifier} from 'ionic-minify';
 let config: IMConfig = require("../minify-conf.json");
 let cmd: string         = process.env.CORDOVA_CMDLINE;
 let rootDir: string     = process.argv[2];
-let minify: boolean     = config.alwaysRun || (cmd.indexOf("--release") > -1);
+let minify: boolean     = config.alwaysRun || (cmd.indexOf("--minify") > -1);
 let platforms: string[] = process.env.CORDOVA_PLATFORMS.split(',');
 let platformPath: string= path.join(rootDir, "platforms");
 
